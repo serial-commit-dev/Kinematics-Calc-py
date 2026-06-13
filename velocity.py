eda197ff7900bd51ae4af648732225efff5f68ca
Neeraj Shibu <neerajms2009@gmail.com>
2026-06-13 08:15:13 +0530

    Add velocity calculation based on user input

--- velocity.py
+++ velocity.py
@@ -1,1 +1,12 @@
-"# Velocity Equation" 
+# Velocity Equation
+# v = u + at
+
+u = int(input("Enter initial velocity: "))
+a = int(input("Enter acceleration: "))
+t = int(input("Enter time(s): "))
+
+def calculate():
+  result = u + a * t
+  print(result,"m/s")
+
+calculate()
